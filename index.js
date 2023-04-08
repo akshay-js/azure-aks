e60e3d9d178063a176e9c8b2df429c24b7da16fb
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-08 21:59:06 +0530

    Port no changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 console.log(`Nodejs started on 3000 top`);
 //create a server object:
 http.createServer(function (req, res) {
-  console.log(`Nodejs started on 3000 top inner`);
+  console.log(`Nodejs started on 8080 top inner`);
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
