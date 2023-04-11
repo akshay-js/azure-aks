a6358f5b4a2afb55b0d97f0939f0762d9144bcd8
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-11 21:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 8080;
 
 app.get('/azure-aks', (req, res) => {
   res.send('Hello World! from azure-aks service')
-})
+});
+
+app.get('/hello-world', async (req, res) => {
+  console.log('Query string', req.query.url);
+   try {
+    const response = await axios.get(`${req.query.url}`);
+    console.log(response);
+     return response;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
