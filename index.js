d6bf8a884844ff04cee42f9b7b91b33ece8d89f2
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-11 21:41:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.get('/hello-world', async (req, res) => {
   console.log('Query string', req.query.url);
    try {
     const response = await axios.get(`${req.query.url}`);
-    console.log(response);
-     return response;
+    console.log(response.data);
+     res.send(response.data);
   } catch (error) {
     console.error(error);
-    return error;
+    res.send(error);
   }
 });
 
